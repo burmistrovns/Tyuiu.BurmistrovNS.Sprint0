fc3c60edd1ae2e6dc7fc70a1b7a95bbaa3aa113b
Николай Бурмистров <Tyuiu.BurmistrovNS@outlook.com>
2025-09-11 00:26:26 +0500

    Task3

---
+++ Tyuiu.Burmistrovns.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.Burmistrovns.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.Burmistrovns.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid1()
+        {
+            Assert.AreEqual(10, DataServiceTest.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.Burmistrovns.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.Burmistrovns.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.Burmistrovns.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ce82e51-c3cb-4dcb-8309-703a41272126")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
